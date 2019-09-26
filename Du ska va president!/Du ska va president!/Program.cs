6571e6bd61149f638824aaed110918f399bec864
oliver176 <55237259+oliver176@users.noreply.github.com>
2019-09-26 13:10:32 +0200

    Intial president commit

---
+++ Du ska va president!/Du ska va president!/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Du_ska_va_president_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
